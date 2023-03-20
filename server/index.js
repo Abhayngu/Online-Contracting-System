5870fbd25fa71e160e2834290695dae5b423a723
Shubham Rathore <shubham.rathore.1831@gmail.com>
2023-03-20 23:17:56 +0530

    added some files

--- server/index.js
+++ server/index.js
@@ -1,16 +1,45 @@
 const express = require('express');
-const dotenv = require('dotenv');
-
 const app = express();
 
-// body parser
-app.use(express.json());
+const path = require('path');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-// Setting up dotenv path
-dotenv.config({ path: './config/config.env' });
+const userRoutes = require('./routes/partyRoutes');
+const User = require('./models/Partymodel');
 
-// Starting server
-app.listen(
-	process.env.PORT,
-	console.log(`server started at port ${process.env.PORT}`)
-);
+
+mongoose.connect(process.env.DB_CONNECTION).then((ans) => {
+    console.log("ConnectedSuccessful")
+  }).catch((err) => {
+    console.log(err)
+  })
+
+app.use(session({
+    secret : 'screte key',
+    resave : false,
+    saveUninitialized : true
+}));
+
+app.use(passport.session());
+app.use(passport.initialize());
+passport.use(new LocalStrategy({usernameField : 'email'}, User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(userRoutes);
+
+app.listen(process.env.PORT, ()=> {
+    console.log('Server is started');
+});
