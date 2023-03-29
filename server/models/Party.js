a8a44763ddf2ac06e2dfff8f297d6244b4eca799
Abhayngu <masterdwivedi@gmail.com>
2023-03-30 00:50:44 +0530

    made project schema and modified party schema

--- server/models/Party.js
+++ server/models/Party.js
@@ -1,28 +1,60 @@
 const mongoose = require('mongoose');
 
-const PartySchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true,
-		unique: true,
+const PartySchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+		},
+		isAnonymous: {
+			type: Boolean,
+			default: false,
+		},
+		isValidator: {
+			type: Boolean,
+			default: false,
+		},
+		tokens: {
+			type: Number,
+			default: 0,
+		},
+		rating: {
+			type: Number,
+			min: 0,
+			max: 5,
+		},
+		projectsDone: {
+			type: Number,
+			default: 0,
+		},
+		projectFailed: {
+			type: Number,
+			default: 0,
+		},
+		projectProposed: {
+			type: [mongoose.Schema.ObjectId],
+			ref: 'Project',
+			default: [],
+		},
+		projectBidFor: {
+			type: [mongoose.Schema.ObjectId],
+			ref: 'Project',
+			default: [],
+		},
+		biddingWon: {
+			type: [mongoose.Schema.ObjectId],
+			ref: 'Project',
+			default: [],
+		},
 	},
-	tokens: {
-		type: Number,
-		default: 0,
-	},
-	rating: {
-		type: Number,
-		min: 0,
-		max: 5,
-	},
-	projectsDone: {
-		type: Number,
-		default: 0,
-	},
-	email: {
-		type: String,
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 module.exports = mongoose.model('Party', PartySchema);
---
+++ server/models/Project.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		description: {
+			type: String,
+			maxLength: 200,
+		},
+		proposedBy: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'Party',
+			require: true,
+		},
+		proposedAt: {
+			type: Date,
+			default: Date.now,
+			immutable: false,
+		},
+		expectedFinishTime: {
+			type: Date,
+		},
+		expectedTokens: {
+			type: Number,
+		},
+		bidders: {
+			type: [mongoose.Schema.ObjectId],
+			ref: 'Party',
+		},
+		isValidated: {
+			type: Boolean,
+			default: false,
+		},
+		isIssued: {
+			type: Boolean,
+			default: false,
+		},
+		wonBy: {
+			type: [mongoose.Schema.ObjectId],
+			ref: 'Party',
+		},
+		milestonesAchieved: {
+			type: string,
+			default: 'Nothing',
+			enum: ['Nothing', 'Design', 'Code', 'Test', 'Deploy'],
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model('Project', ProjectSchema);
