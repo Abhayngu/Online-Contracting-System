49715f1f273a74f6318a62364670ee7633d34e79
Abhayngu <masterdwivedi@gmail.com>
2023-03-30 10:07:30 +0530

    connected database to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -9,8 +10,16 @@ app.use(express.json());
 // Setting up dotenv path
 dotenv.config({ path: './config/config.env' });
 
+// Connecting to data base
+const connectToDataBase = async () => {
+	const c = await mongoose.connect(process.env.DB, {});
+	console.log('Database connected ', c.connection.host);
+};
+
 // Starting server
 app.listen(
 	process.env.PORT,
 	console.log(`server started at port ${process.env.PORT}`)
 );
+
+connectToDataBase();
