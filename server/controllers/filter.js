dd74bb937c058b0a72d48d93f6ac4a946a0d5f26
Hrithik Lal <hrithik.lal2012@gmail.com>
2023-04-01 11:05:44 +0530

    added getTop3Project,getAllProjects,getProjectByEmail functions

---
+++ server/controllers/filter.js
@@ -0,0 +1,33 @@
+const Project = require('../models/Project');
+
+exports.getTop3Projects = async (req, res, next) => {
+    const projects = await Project.find().sort({ finalBidPrice: -1 }).limit(3);
+    res.status(200).json({data: projects});
+};
+
+exports.getAllProjects = async ( req, res, next) => {
+    const projects = await Project.find();
+    res.status(200).json({data: projects});
+};
+
+exports.getProjectsByEmail = async (req, res, next) => {
+    try {
+        const projects = await Project.find({ email: req.params.email});
+        if(!projects) {
+            return res.status(404).json({ msg: 'No projects found for this email'});
+        }
+        res.status(200).json({data: projects});
+    } catch (err) {
+        res.status(400).json({ msg: 'Server error'});
+    }
+};
+
+exports.getUserProjects = async (req, res, next) => {
+    try {
+      const user = req.user; // get the authenticated user from the request object
+      const projects = await Project.find({ userId: user.id }); // find all projects associated with the user
+      res.status(200).json({ data: projects });
+    } catch (err) {
+      res.status(500).json({ msg: 'Server error' });
+    }
+  };
