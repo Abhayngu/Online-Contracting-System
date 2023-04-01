6ad93e8864432c1ea698653392943322a45b8121
Hrithik Lal <hrithik.lal2012@gmail.com>
2023-04-01 22:57:20 +0530

    added filter project

--- server/controllers/filter.js
+++ server/controllers/filter.js
@@ -21,3 +21,47 @@ exports.getProjectsByEmail = async (req, res, next) => {
         res.status(400).json({ msg: 'Server error'});
     }
 };
+
+exports.filterProject = async (req, res, next) => {
+    // const projects = await Project.find({ isValidated : false});
+    // res.status(200).json({data: projects});
+
+    let { isValidated, isIssued, milestonesAchieved} = req.body;
+    const condition = {};
+    let query;
+    if( isValidated) {
+        condition['isvalidated'] = isValidated;
+    }
+    if( isIssued) {
+        condition['isIssued'] = isIssued;
+    }
+    if( milestonesAchieved) {
+        condition['type.milestonesAchieved'] = milestonesAchieved;
+    }
+    query = Project.find(condition);
+    let total = await query;
+    total = total.length;
+
+    if(!page) {
+        page = 1;
+    }
+    const limit = 2;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    query = query.skip(startIndex).limit(limit);
+    const projects = await query;
+    const totalPages = Math.ceil(total / limit);
+	const pagination = {};
+
+	if (endIndex < total) {
+		pagination.next = {
+			page: page + 1,
+		};
+	}
+	if (startIndex > 0) {
+		pagination.prev = {
+			page: page - 1,
+		};
+	}
+    res.status(200).json({data: projects});
+};
