9b9f410021c896f2c012c92a1c61d29e66c6affb
Abhayngu <masterdwivedi@gmail.com>
2023-04-04 11:29:19 +0530

    editing project box

--- client/src/Components/ProjectBox.js
+++ client/src/Components/ProjectBox.js
@@ -2,17 +2,33 @@ import React, { useState, useEffect } from 'react';
 
 function ProjectBox({ id }) {
 	const [proId, setProId] = useState(34);
+	const [projectName, setProjectName] = useState('laptop bidding');
+	const [projectStatus, setProjectStatus] = useState('Bidding going on');
+	const [proposedBy, setProposedBy] = useState('party3');
 	const customStyle = {
-		projectBox: { backgroundColor: '#fbfbfb' },
+		projectBox: {
+			backgroundColor: '#A9EDC4',
+			display: 'flex',
+			flexDirection: 'column',
+			justifyContent: 'center',
+			alignItems: 'center',
+			width: '200px',
+			height: '200px',
+			overflow: 'hidden',
+		},
 		projectId: {},
 		projectName: {},
 		projectStatus: {},
+		projectProposedBy: {},
 	};
 	return (
 		<React.Fragment>
 			<div style={customStyle.projectBox}>
-				<div style={customStyle.projectId}>Id : {id}</div>
-				<div></div>
+				<div style={customStyle.projectId}>Id : {proId}</div>
+				<div style={customStyle.projectName}>Name : {projectName}</div>
+				<div style={customStyle.projectName}>
+					Status : {projectName}
+				</div>
 			</div>
 		</React.Fragment>
 	);
