05b1b944dd6cac83d91bd3b20249431739e2fc8f
Abhayngu <masterdwivedi@gmail.com>
2023-04-05 20:54:23 +0530

    made profile page

--- client/src/Components/ProjectBox.js
+++ client/src/Components/ProjectBox.js
@@ -1,33 +1,57 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function ProjectBox({ id }) {
-	const [proId, setProId] = useState(34);
-	const [projectName, setProjectName] = useState('laptop bidding');
+	const navigate = useNavigate();
+	const [proId, setProId] = useState(id);
+	const [projectName, setProjectName] = useState('Laptop bidding');
 	const [projectStatus, setProjectStatus] = useState('Bidding going on');
 	const [proposedBy, setProposedBy] = useState('party3');
+	function handleProjectClick() {
+		navigate(`/project?id=${proId}`);
+	}
 	const customStyle = {
 		projectBox: {
 			backgroundColor: '#A9EDC4',
-			display: 'flex',
-			flexDirection: 'column',
-			justifyContent: 'center',
-			alignItems: 'center',
 			width: '200px',
 			height: '200px',
 			overflow: 'hidden',
+			padding: '10px',
+			textAlign: 'center',
+			cursor: 'pointer',
+			borderRadius: '20px',
+			paddingTop: '30px',
+			marginRight: '20px',
+		},
+		projectFlex: {
+			marginTop: '20px',
+			display: 'flex',
+			flexDirection: 'column',
+			justifyContent: 'flex-end',
+			alignItems: 'center',
 		},
 		projectId: {},
-		projectName: {},
+		projectName: {
+			selfAlign: 'flex-start',
+			fontSize: '16px',
+			fontWeight: '600',
+		},
 		projectStatus: {},
 		projectProposedBy: {},
 	};
 	return (
 		<React.Fragment>
-			<div style={customStyle.projectBox}>
-				<div style={customStyle.projectId}>Id : {proId}</div>
-				<div style={customStyle.projectName}>Name : {projectName}</div>
-				<div style={customStyle.projectName}>
-					Status : {projectName}
+			<div onClick={handleProjectClick} style={customStyle.projectBox}>
+				<div style={customStyle.projectName}>{projectName}</div>
+				<div style={customStyle.projectFlex}>
+					<div style={customStyle.projectStatus}>
+						<span style={{ color: 'red' }}>Status</span> :{' '}
+						{projectStatus}
+					</div>
+					<div style={customStyle.projectProposedBy}>
+						<span style={{ color: 'blue' }}>Proposed By</span> :{' '}
+						{proposedBy}
+					</div>
 				</div>
 			</div>
 		</React.Fragment>
--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../Components/Header';
+import ProjectBox from '../Components/ProjectBox';
 
 function Profile() {
 	const queryParameters = new URLSearchParams(window.location.search);
@@ -10,24 +11,75 @@ function Profile() {
 		'decoration system',
 		'roadways',
 	]);
-	const [myProject, setMyProject] = useState(['mypro1', 'mypro2']);
+	const [myProject, setMyProject] = useState([
+		{ id: 54, name: 'sdafdsf' },
+		{ id: 5463, name: 'galjesdf' },
+	]);
 	const [tokens, setTokens] = useState(30);
 	const [isValidator, setIsValidator] = useState(false);
 
-	const customStyle = {};
+	const customStyle = {
+		profileContainer: {
+			// display: 'flex',
+			// justifyContent: 'center',
+			// textAlign: 'center',
+			padding: '80px 40px 20px 50px',
+			width: '100%',
+		},
+		partyName: {
+			textDecoration: 'underline',
+			fontSize: '40px',
+			marginBottom: '45px',
+			// marginBottom: '30px',
+			color: '#46B6A6',
+			// fontSize: '16px',
+		},
+		tokens: {
+			marginBottom: '30px',
+			fontSize: '16px',
+		},
+		isValidator: {
+			marginBottom: '30px',
+			fontSize: '16px',
+		},
+		projectHeading: {
+			marginBottom: '30px',
+			fontWeight: '600',
+		},
+		myProjectsContainer: {
+			display: 'flex',
+			justifyContent: 'flex-start',
+			marginBottom: '30px',
+		},
+		projectBidContainer: {
+			display: 'flex',
+			justifyContent: 'flex-start',
+		},
+	};
 	return (
 		<React.Fragment>
 			<Header c="#d9d9d9" />
-			<div style={customStyle.projectContainer}>
-				<div style={customStyle.party}>{name}</div>
-				<div style={customStyle.projectId}>Tokens : {tokens}</div>
-				<div style={customStyle.proposedBy}>
-					Is a Validator? : {isValidator}
+			<div style={customStyle.profileContainer}>
+				<div style={customStyle.partyName}>{name}</div>
+				<div style={customStyle.tokens}>Tokens : {tokens}</div>
+				<div style={customStyle.isValidator}>
+					Is a Validator? :{' '}
+					{isValidator ? (
+						<span style={{ color: 'green' }}>Yes</span>
+					) : (
+						<span style={{ color: 'red' }}>No</span>
+					)}
 				</div>
-				<div style={customStyle.bidWonBy}>
-					My Projects
+				<div style={customStyle.projectHeading}>My Projects</div>
+				<div style={customStyle.myProjectsContainer}>
 					{myProject.map((project) => {
-						return <div style={customStyle.specificProject}></div>;
+						return <ProjectBox id={project.id} />;
+					})}
+				</div>
+				<div style={customStyle.projectHeading}>Projects Bid For</div>
+				<div style={customStyle.projectBidContainer}>
+					{projectBidFor.map((project) => {
+						return <ProjectBox id={project} />;
 					})}
 				</div>
 			</div>
