ab131ce0078e0274472a82ebe29165012b749821
Abhayngu <masterdwivedi@gmail.com>
2023-04-15 23:53:54 +0530

    added delete

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const userRoutes = require('./routes/route');
 const User = require('./models/Party');
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'content-type');
 	next();
 });
