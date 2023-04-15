d0358137ba665cd7ff5e48beef25f8503244fa35
subhajit-6496 <subhajitpanday30@gmail.com>
2023-04-15 18:20:11 +0530

    added validation feature in bid function in smart contract

--- client/src/contracts/Bidding.sol
+++ client/src/contracts/Bidding.sol
@@ -78,7 +78,7 @@ contract Bidding {
         Project storage temp = projects[project_id];
 
         require(temp.exists,"Project Doesn't exist");
-        // require(temp.validated,"Project is not validated yet");
+        require(temp.validated,"Project is not validated yet");
         require(temp.endTime>block.timestamp, "Bidding is ended for this project");
         require(!IsOwner(temp.owner), "Project Issuer is not allowed to bid");
         require(msg.value!=0, "Zero amount is not allowed.");
